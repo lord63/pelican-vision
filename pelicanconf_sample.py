1669157511cacbd322845d9e504d6d5c2e584284
lord63 <lord63.j@gmail.com>
2015-08-13 19:37:45 +0800

    First attemp to port the theme, works now

---
+++ pelicanconf_sample.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+from __future__ import unicode_literals
+
+# The followings are recommanded to set them up.
+AUTHOR = ''
+SITENAME = ''
+SITEURL = ''
+
+PATH = 'content'
+
+DEFAULT_LANG = ''
+
+THEME = ''
+
+TIMEZONE = ''
+
+LINKS = (('About', '#'),
+         ('Projects', '#'),
+         ('Your links', '#'),)
+
+SOCIAL = (('Github', '#'),
+          ('Twitter', '#'),
+          ('Your links', '#'),)
+
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+
+# Let them be or edit as you want.
+DISQUS_SITENAME = ''
+
+GOOGLE_ANALYTICS = ''
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
